7dfafe53cc1008c9a1c028cbb6e14057620f5563
Noah Korner <noahskorner@gmail.com>
2020-05-14 16:06:07 -0500

    Started implementation of AddTodo component

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
-      <h3>Add Todo</h3>
-      <div class="add">
-          <form action="">
-              <input type="text" v-model="title" placeholder="Add Todo...">
-              <input type="submit" value="Submit">
-          </form>
-      </div>
+    <h3>Add Todo</h3>
+    <div class="add">
+      <form @submit="onSubmit">
+        <input type="text" v-model="title" placeholder="Add Todo...">
+        <input type="submit" value="Submit">
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
-    name: "AddTodo"
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        ...mapActions(['addTodo']),
+        onSubmit(e){
+            e.preventDefault();
+            this.addTodo(this.title);
+        }
+    }
 }
 </script>
 
